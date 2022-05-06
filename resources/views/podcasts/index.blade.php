0c04e0d5485c3479027de0f8cd8222845625ec4f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-06 14:23:09 +0200

    podcasts: design the podcasts list

--- resources/views/podcasts/index.blade.php
+++ resources/views/podcasts/index.blade.php
@@ -1,18 +1,16 @@
 <x-app-layout>
     <h1>Podcasts</h1>
-    <h3>Parcourez et découvrez tous les podcasts publiés sur Podz.</h3>
-
-    <div class="flex">
+    <p>Parcourez et découvrez tous les podcasts publiés sur Podz.</p>
+    <hr class="border-blue my-3">
+    <div class="flex flex-wrap">
         @foreach($podcasts as $podcast)
-        <div class="border">
-            <div>{{ $podcast->title }}</div>
+        <div class=" m-1 border border-blue rounded w-auto min-w-[] max-w-xs px-2 py-1">
+            <h2>{{ $podcast->title }}</h2>
             <div class="flex">
-                <div class="flex-1">{{ $podcast->author->name }}</div>
-                <div>{{ $podcast->episodes->count() }}</div>
+                <div class="flex-1 font-semibold">{{ $podcast->author->name }}</div>
+                <div class="text-green italic text-sm">{{ $podcast->episodes->count() . ' épisode' . ( $podcast->episodes->count() > 1 ? 's' : ' ') }}</div>
             </div>
-            <div class="h-10 overflow-ellipsis">
-                <div>{{ $podcast->description }}</div>
-            </div>
+            <div class="mt-1 text-sm italic text-gray-700 overflow-hidden text-ellipsis">{{ $podcast->resume }}</div>
         </div>
         @endforeach
     </div>
