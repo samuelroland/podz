27a3691548a9a50342c9460b2284068e0cc0f8c8
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-06 11:17:46 +0200

    podcasts: write html structure and data

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Podcast;
 use Illuminate\Http\Request;
 
 class PodcastController extends Controller
 {
     public function index()
     {
-        return view('podcasts.index');
+        $podcasts = Podcast::all();
+        return view('podcasts.index', ['podcasts' => $podcasts]);
     }
 }
--- resources/views/podcasts/index.blade.php
+++ resources/views/podcasts/index.blade.php
@@ -1,3 +1,19 @@
 <x-app-layout>
-    test index podcasts
+    <h1>Podcasts</h1>
+    <h3>Parcourez et découvrez tous les podcasts publiés sur Podz.</h3>
+
+    <div class="flex">
+        @foreach($podcasts as $podcast)
+        <div class="border">
+            <div>{{ $podcast->title }}</div>
+            <div class="flex">
+                <div class="flex-1">{{ $podcast->author->name }}</div>
+                <div>{{ $podcast->episodes->count() }}</div>
+            </div>
+            <div class="h-10 overflow-ellipsis">
+                <div>{{ $podcast->description }}</div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </x-app-layout>
