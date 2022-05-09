21262483233bbfe29073f0e82c2e1534bbcc5552
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-09 15:15:04 +0200

    EpisodeFactory: enhance dates generation

--- database/factories/EpisodeFactory.php
+++ database/factories/EpisodeFactory.php
@@ -18,15 +18,17 @@ class EpisodeFactory extends Factory
         $title = $this->faker->text(36);    //35 + a dot (that will be removed)
         $title = Str::substr($title, 0, Str::length($title) - 1);   //remove the last "." at the end
 
-        //Generate release date, and make sure if an episode 
+        //Todo: Generate release date, and make sure an episode is always after the date of the last one
 
+        $releaseDate = now()->subDays(rand(1, 50))->addMinutes(rand(60, 1000))->addHours(rand(1, 5))->addSeconds(rand(1, 45));
         return [
             'title' => $title,
             'description' => $this->faker->text(rand(20, 2000)),
             'number' => $this->number++,
             'hidden' => rand(0, 10) == 10 ? true : false,
-            'released_at' => now()->subDays(rand(1, 50)),
+            'released_at' => $releaseDate,
             'filename' => Str::random(50),
+            'created_at' => rand(0, 1) == 0 ? $releaseDate : $releaseDate->subHours(rand(0, 12))
         ];
     }
 }
