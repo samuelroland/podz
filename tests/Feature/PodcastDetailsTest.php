53dd0317c30552b7649903651f77e0179f12e08b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-09 12:11:53 +0200

    PodcastDetailsTest: add 2 basic tests

---
+++ tests/Feature/PodcastDetailsTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Episode;
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Podcast;
+
+class PodcastsDetailsTest extends TestCase
+{
+    public function test_podcasts_details_page_exists()
+    {
+        $response = $this->get(route('podcasts.show', Podcast::first()->id));
+
+        $response->assertStatus(200);
+    }
+
+    //Author view
+    public function test_all_information_are_displayed()
+    {
+        $podcast = Podcast::first();
+
+        $response = $this->actingAs($podcast->author)->get(route('podcasts.show', $podcast->id));
+
+        //Make sure podcast info are present
+        $response->assertSee($podcast->title);
+        $response->assertSee("par " . $podcast->author);
+        $response->assertSee($podcast->description);
+
+        $this->assertNotEmpty($podcast->episodes);  //question: is it ok ?
+
+        //Make sure episodes info are present
+        foreach ($podcast->episodes as $episode) {
+            $response->assertSee("#" . $episode->number);
+            $response->assertSee($episode->title);
+            $response->assertSee($episode->description);
+            $response->assertSee($episode->path);
+            $response->assertSee("Publié le " . $episode->released_at->format("d.m.Y H:i"));
+            $response->assertSee("Créé le " . $episode->released_at->format("d.m.Y à H:i"));
+        }
+    }
+
+    public function test_make_sure_icons_are_present()
+    {
+        //todo
+    }
+
+    public function test_release_date_in_futur_is_displayed_correctly()
+    {
+        //todo
+    }
+}
