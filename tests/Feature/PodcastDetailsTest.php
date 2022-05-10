13ad3262eeb4ddf1d12d3dd74beff4da2a5b79d4
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-10 16:41:55 +0200

    asdf

--- tests/Feature/PodcastDetailsTest.php
+++ tests/Feature/PodcastDetailsTest.php
@@ -28,14 +28,14 @@ class PodcastsDetailsTest extends TestCase
         $response->assertSee("par " . $podcast->author->name);
         $response->assertSee($podcast->description);
 
-        $this->assertNotEmpty($podcast->episodes);  //question: is it ok ? always true if seeder doesn't change
+        $this->assertNotEmpty($podcast->episodes);
 
         //Make sure episodes info are present
         foreach ($podcast->episodes as $episode) {
             $response->assertSee("#" . $episode->number);
             $response->assertSee($episode->title);
             $response->assertSee($episode->description);
-            $response->assertSee($episode->path);
+            $response->assertSee($episode->path);   //present in <audio>
             $response->assertSee("Publié le " . $episode->released_at->format("d.m.Y à H:i"));
             $response->assertSee("Créé le " . $episode->created_at->format("d.m.Y à H:i"));
         }
@@ -43,7 +43,11 @@ class PodcastsDetailsTest extends TestCase
 
     public function test_make_sure_icons_are_present()
     {
-        //todo
+        $podcast = Podcast::first();
+
+        $response = $this->actingAs($podcast->author)->get(route('podcasts.show', $podcast->id));
+
+        $response->assertSee("");
     }
 
     public function test_release_date_in_futur_is_displayed_correctly()
