aae53334c0cd4a0a501734c889e2c4630941bb1c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-12 12:15:04 +0200

    web: add routes for create and store under sanctum middleware

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,10 @@ use App\Http\Controllers\PodcastController;
 
 Route::controller(PodcastController::class)->group(function () {
     Route::get('/', 'index')->name('podcasts.index');
+
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/podcasts/create', 'create')->name('podcasts.create');
+        Route::post('/', 'store')->name('podcasts.store');
+    });
     Route::get('/podcasts/{podcast}', 'show')->name('podcasts.show');
 });
