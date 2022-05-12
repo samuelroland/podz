30080859ff52c7002b1fd088fd80047e15c103c5
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-12 12:14:04 +0200

    PodcastController: implement create() and store()

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Podcast;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PodcastController extends Controller
 {
@@ -18,5 +19,29 @@ class PodcastController extends Controller
         //$isAuthor = auth()->check() && $podcast->author->is(auth()->user());
 
         return view('podcasts.show', ['podcast' => $podcast]);
+    }
+
+    public function create()
+    {
+        return view('podcasts.create');
+    }
+
+    public function store()
+    {
+        //TODO: refactor logic to make it shorter (maybe with mass assignation ?)
+        $data = request()->all();
+
+        Validator::make($data, [
+            'title' => 'required|max:35',
+            'description' => 'required|max:2000',
+        ])->validate();
+
+        $podcast = new Podcast();
+        $podcast->title = $data['title'];
+        $podcast->description = $data['description'];
+        $podcast->user_id = auth()->id();
+        $podcast->save();
+
+        return redirect(route('podcasts.index'));
     }
 }
