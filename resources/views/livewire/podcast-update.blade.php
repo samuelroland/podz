81bee1c18becb15e0be0c160c8c09df597e2b000
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-13 11:33:16 +0200

    podcast: use new placeholder prop of field

--- resources/views/livewire/podcast-update.blade.php
+++ resources/views/livewire/podcast-update.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <x-field wire:keyup.enter="update" label="Title" name="podcast.title" wire:model.lazy="podcast.title"></x-field>
-    <x-field label="Description" type="textarea" name="podcast.description" wire:model.lazy="podcast.description">
+    <x-field wire:keyup.enter="update" placeholder="Rentrez un titre court et marquant." label="Title" name="podcast.title" wire:model.lazy="podcast.title"></x-field>
+    <x-field label="Description" placeholder="De quoi parlera votre podcast ?" type="textarea" name="podcast.description" wire:model.lazy="podcast.description">
     </x-field>
     @csrf
     <button wire:click.prevent="update" class="btn mt-1">Enregistrer</button>
--- resources/views/podcasts/create.blade.php
+++ resources/views/podcasts/create.blade.php
@@ -2,10 +2,16 @@
     <h1 class="flex-1">Créer un podcast</h1>
     <div>
         <form action="{{ route('podcasts.index') }}" method="POST">
-            <x-field label="Title" name="title"></x-field>
-            <x-field label="Description" type="textarea" name="description"></x-field>
+            <x-field label="Title" placeholder="Rentrez un titre court et marquant." cc name="title"></x-field>
+            <x-field label="Description" placeholder="De quoi parlera votre podcast ?" type="textarea" name="description"></x-field>
             @csrf
             <input type="submit" value="Créer" class="btn mt-1 ">
         </form>
+    </div>
+
+    <div class="whitespace-pre">
+        ajskdflsjakldfésda
+        asdf
+        sad
     </div>
 </x-app-layout>
