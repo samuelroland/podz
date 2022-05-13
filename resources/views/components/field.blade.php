c85c3451891f875d15cbb0b4c1f5f988cb62b30d
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-13 11:32:58 +0200

    field: add new prop "placeholder"

--- resources/views/components/field.blade.php
+++ resources/views/components/field.blade.php
@@ -1,13 +1,13 @@
-@props(['label' => '', 'name', 'type' => 'text', 'cssOnField' => ''])
+@props(['label' => '', 'name', 'type' => 'text', 'cssOnField' => '', 'placeholder' => ''])
 <div {{ $attributes->whereDoesntStartWith('wire:model')->merge(['class' => 'mt-3']) }}>
     @if($label != '')
     <label class="mb-1 block">{{ $label }}</label>
     @endif
 
     @if ($type != 'textarea')
-    <input {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
+    <input {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 p-0 h-8 " name="{{ $name }}" />
     @else
-    <textarea {{ $attributes->whereStartsWith('wire:model') }} type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
+    <textarea {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
     @endif
 
     @error($name)
