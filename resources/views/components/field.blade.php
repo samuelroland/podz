8bffe1d7013bbc9020694a6752692835a9d995ba
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-17 12:18:34 +0200

    field: make sure the error message is displayed under the input

--- resources/views/components/field.blade.php
+++ resources/views/components/field.blade.php
@@ -4,16 +4,18 @@
     <label class="mb-1 block">{{ $label }}</label>
     @endif
 
-    @if ($type != 'textarea')
-    <input {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 {{ in_array($type, ['checkbox']) ? '' : 'h-8' }}" name="{{ $name }}" />
-    @else
-    <textarea {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
-    @endif
+    <div>
+        @if ($type != 'textarea')
+        <input {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 {{ in_array($type, ['checkbox']) ? '' : 'h-8' }}" name="{{ $name }}" />
+        @else
+        <textarea {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
+        @endif
 
-    @error($name)
-    <div class="text-red-500 italic text-sm">
-        {{ $message }}
+        @error($name)
+        <div class="text-red-500 italic text-sm">
+            {{ $message }}
+        </div>
+        @enderror
     </div>
-    @enderror
 
 </div>
