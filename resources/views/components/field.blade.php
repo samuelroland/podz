933ae9de9601a357ed0fa3fbca8637b8ccd43a6f
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-20 13:55:05 +0200

    field: small fix on progress bar

--- resources/views/components/field.blade.php
+++ resources/views/components/field.blade.php
@@ -8,11 +8,11 @@
         @if($type == 'file')
         {{-- Progress bar code taken from the Livewire docs: https://laravel-livewire.com/docs/2.x/file-uploads#js-hooks --}}
 
-        <div x-data="{ isUploading: false, progress: 0, failed: false }" x-on:livewire-upload-start="isUploading = true; uploadCompleted = false" x-on:livewire-upload-error="isUploading = false" x-on:livewire-upload-progress="progress = $event.detail.progress" x-on:livewire-upload-finish="uploadCompleted = true">
+        <div x-data="{ isUploading: false, progress: 0, failed: false }" x-on:livewire-upload-start="isUploading = true; uploadCompleted = false" x-on:livewire-upload-error="failed = true" x-on:livewire-upload-progress="progress = $event.detail.progress" x-on:livewire-upload-finish="uploadCompleted = true">
             @endif
 
             @if ($type != 'textarea')
-            <input {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 {{ in_array($type, ['checkbox']) ? '' : 'h-8' }}" name="{{ $name }}" {{ $type=='file' ? '\@change="isUploading = false"' : '' }} />
+            <input {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" value="{{ old($name) }}" class="{{ $cssOnField }} rounded-md px-1 border border-gray-400 {{ in_array($type, ['checkbox']) ? '' : 'h-8' }}" name="{{ $name }}" {{ $type=='file' ? '\@change="isUploading = false; failed = false"' : '' }} />
             @else
             <textarea {{ $attributes->whereStartsWith('wire:model') }} placeholder="{{ $placeholder }}" type="{{ $type }}" class="{{ $cssOnField }} rounded-md border border-gray-400 p-1 w-full md:w-2/3 lg:w-1/2" name="{{ $name }}" rows="3">{{ old($name) }}</textarea>
             @endif
@@ -20,7 +20,7 @@
             @if($type == 'file')
             <!-- Progress Bar -->
             <div x-show="isUploading" x-cloak>
-                Progression: <span class="text-green" x-text="uploadCompleted ? 'Téléversé!' : 'En cours'"></span>
+                Progression: <span class="text-green" x-show="!failed" x-text="uploadCompleted ? 'Téléversé!' : 'En cours'"></span>
                 <span class="text-red-500" x-show="failed">Erreur...</span>
                 <br><progress max="100" x-bind:value="progress" class="w-full"></progress>
             </div>
