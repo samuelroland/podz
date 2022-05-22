363b0770a560956563b004ff7c2e13080c3c7139
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-22 22:38:57 +0200

    EpisodeUpdate: create empty livewire component

---
+++ app/Http/Livewire/EpisodeUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class EpisodeUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.episode-update');
+    }
+}
