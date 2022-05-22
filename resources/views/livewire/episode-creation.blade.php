a3e8295a48db84a4a87a038e6ff74e95dbab632a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-23 00:52:34 +0200

    EpisodeCreation: use EpisodeForm component

--- resources/views/livewire/episode-creation.blade.php
+++ resources/views/livewire/episode-creation.blade.php
@@ -2,20 +2,8 @@
     <h2 :class="{'cursor-pointer hover:underline': !newEpisodeOpen}" @click="newEpisodeOpen = true">Nouvel épisode <span x-show="!newEpisodeOpen">...</span></h2>
     <div x-show="newEpisodeOpen" class="border-l border-blue px-2 pl-3 my-5 w-full ">
 
-        <div class="flex items-center">
-            <span class="text-2xl mr-2 mt-3">{{ '#' . $episode->number }}</span>
-            <x-field wire:model.lazy="episode.title" name="episode.title" placeholder="Titre de l'épisode"></x-field>
-            <x-field wire:model="episode.hidden" name="episode.hidden" cssOnField="ml-2 block" label="Caché" class="flex items-center mx-2" type="checkbox"></x-field>
-            <x-field wire:model.defer="datetime" name="datetime" type="datetime-local"></x-field>
-        </div>
+        <x-episode-form :episode="$episode" mode="create"></x-episode-form>
 
-        <div>
-            <x-field wire:model.lazy="episode.description" name="episode.description" type="textarea" placeholder="Description de l'épisode"></x-field>
-        </div>
-
-        <div class="flex items-center">
-            <x-field wire:model="file" name="file" cssOnField="max-w-lg" label="Fichier audio (.mp3, .ogg ou .opus). Pas plus de 150MB." type="file"></x-field>
-        </div>
         <div class="mt-2">
             <button class="btn" wire:click="publish()">Sauver</button>
             @if(session()->has('newEpisode'))
