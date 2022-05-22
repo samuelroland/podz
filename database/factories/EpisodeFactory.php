e1c77fb9dd02b435b556dceabd17e43df59ecfe0
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-22 19:07:59 +0200

    EpisodeFactory: make sure episodes are public for testing

--- database/factories/EpisodeFactory.php
+++ database/factories/EpisodeFactory.php
@@ -20,14 +20,17 @@ class EpisodeFactory extends Factory
         $title = Str::substr($title, 0, Str::length($title) - 1);   //remove the last "." at the end
 
         //Todo: Generate release date, and make sure an episode is always after the date of the last one
+        //TODO: make sure the release date is in the past.
+
+        $testing = app()->environment('testing');   //during testing, we want to make sure episodes are publicly visible by default
 
         $releaseDate = now()->subDays(rand(1, 50))->addMinutes(rand(60, 1000))->addHours(rand(1, 5))->addSeconds(rand(1, 45));
         return [
             'title' => $title,
             'description' => $this->faker->text(rand(20, 2000)),
             'number' => $this->number++,
-            'hidden' => rand(0, 10) == 10 ? true : false,
-            'released_at' => $releaseDate,
+            'hidden' => $testing ? false : rand(0, 10) == 10,
+            'released_at' => $testing ? now()->subDays(rand(1, 50)) : $releaseDate,
             'filename' => Str::random(50) . "." . (['mp3', 'ogg', 'opus'])[rand(0, 2)], //choose a random format among available ones
             'created_at' => rand(0, 1) == 0 ? $releaseDate : $releaseDate->subHours(rand(0, 12))
         ];
