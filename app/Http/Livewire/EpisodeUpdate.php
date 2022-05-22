1c04b8b672de772137a52017eb71bbe5ab581c10
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-23 00:51:57 +0200

    EpisodeUpdate: use EpisodeForm component

--- app/Http/Livewire/EpisodeUpdate.php
+++ app/Http/Livewire/EpisodeUpdate.php
@@ -6,8 +6,21 @@ use Livewire\Component;
 
 class EpisodeUpdate extends Component
 {
+    public $episode;
+    public $datetime;   //bridge between episode.released_at and the view to manage format change
+
     public function render()
     {
         return view('livewire.episode-update');
+    }
+
+    public function mount()
+    {
+        $this->datetime = $this->episode->released_at;
+    }
+
+    public function updated($field)
+    {
+        $this->validateOnly($field);
     }
 }
--- resources/views/livewire/episode-update.blade.php
+++ resources/views/livewire/episode-update.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <x-episode-form :episode="$episode" mode="update"></x-episode-form>
+    
+    <div class="mt-2">
+        <button class="btn" wire:click="updateEpisode()" @click="edition = false">Mettre à jour</button>
+        @if(session()->has('updatedEpisode'))
+        <span class="text-green">
+            L'épisode '{{ session('updatedEpisode') }}' a bien été mis à jour.
+        </span>
+        @endif
+    </div>
 </div>
