52662a15bde73d3b223d9ee1581f8e21c3b6df44
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-23 09:08:43 +0200

    EpisodeUpdate: add working updateEpisode() and rules()

--- app/Http/Livewire/EpisodeUpdate.php
+++ app/Http/Livewire/EpisodeUpdate.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
+use App\Models\Episode;
 use Livewire\Component;
+use Illuminate\Validation\Rule;
 
 class EpisodeUpdate extends Component
 {
+    public $podcast;
     public $episode;
     public $datetime;   //bridge between episode.released_at and the view to manage format change
+
+    //As the rules are dynamically generated (contains an ID here), we need to wrap them in this function instead of $rules
+    protected function rules()
+    {
+        return [
+            'episode.title' => [
+                'required',
+                'max:60',
+                Rule::unique(with(new Episode)->getTable(), 'title')->where(fn ($query) => $query->where('podcast_id', $this->podcast->id))->ignore($this->episode->id),
+            ],
+            'episode.description' => 'required|max:2000',
+            'episode.hidden' => 'boolean',
+            'datetime' => 'required|date',
+        ];
+    }
 
     public function render()
     {
@@ -22,5 +41,20 @@ class EpisodeUpdate extends Component
     public function updated($field)
     {
         $this->validateOnly($field);
+    }
+
+    public function updateEpisode()
+    {
+        $this->validate();
+
+        if ($this->podcast->isAuthor()) {
+            $this->episode->released_at = Carbon::parse($this->datetime);
+
+            $this->episode->save();
+
+            $this->emit('episodesListUpdate');
+
+            session()->flash('updatedEpisode', "#" . $this->episode->number . " " . $this->episode->title);
+        }
     }
 }
