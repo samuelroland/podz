7283935554880126d9935a59ba3ccf43e39976c8
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-30 00:24:00 +0200

    PodcastCreation: fix the name of the route for the form

--- resources/views/podcasts/create.blade.php
+++ resources/views/podcasts/create.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
     <h1 class="flex-1">Créer un podcast</h1>
     <div>
-        <form action="{{ route('podcasts.index') }}" method="POST">
+        <form action="{{ route('podcasts.store') }}" method="POST">
             <x-field label="Title" placeholder="Rentrez un titre court et marquant." cc name="title"></x-field>
-            <x-field label="Description" placeholder="De quoi parlera votre podcast ?" type="textarea" name="description"></x-field>
+            <x-field label="Description" placeholder="De quoi parlera votre podcast ?" type="textarea"
+                name="description"></x-field>
             @csrf
             <input type="submit" value="Créer" class="btn mt-1 ">
         </form>
