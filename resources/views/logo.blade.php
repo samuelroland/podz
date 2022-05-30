ae2630a1c7144cb745390904ed39af654e9355aa
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-05-30 16:02:43 +0200

    logo: small fix on the color chose

--- resources/views/logo.blade.php
+++ resources/views/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-end">
-    <div class="text-teal-600 text-4xl">
+    <div class="text-green text-4xl">
         Podz
     </div>
     <span class="text-sm italic ml-1" title="La version {{ config('app.version') }} a été publiée le {{ config('app.version_date')->format('d.m.Y') }}.">{{ config('app.version') }}</span>
